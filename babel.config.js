7480e1ac91fc31b64f6282aae1205254af99455f
Kelvin Musselli <kelvinmusselli22@gmail.com>
2020-05-13 21:21:02 -0300

    generator!

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    'babel-plugin-styled-components',
+    'react-hot-loader/babel',
     '@babel/plugin-transform-regenerator',
   ],
 };
