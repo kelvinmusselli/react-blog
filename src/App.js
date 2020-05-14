e5714b3415369e939bcbae84e4c00881572409cd
Kelvin Musselli <kelvinmusselli22@gmail.com>
2020-05-13 22:22:19 -0300

    Add Toast

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import { ToastContainer } from 'react-toastify';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import Routes from './routes';
@@ -9,6 +9,7 @@ import Routes from './routes';
 const App = () => {
   return (
     <Provider store={store}>
+      <ToastContainer autoClose={3000} />
       <GlobalStyle />
       <Header />
       <Routes />
--- src/store/modules/posts/sagas.js
+++ src/store/modules/posts/sagas.js
@@ -1,13 +1,14 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
 import { getPosts } from './actions';
+import { toast } from 'react-toastify';
 
 function* get_post() {
   try {
     const data = yield call(api.get, 'v2/5be5e3ae2f00005b000fc3f6');
     yield put(getPosts(data));
   } catch (err) {
-    console.log('sadsa');
+    toast.error('Desculpe occoreu um erro ao consumir a api');
   }
 }
 
